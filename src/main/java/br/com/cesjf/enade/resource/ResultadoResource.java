1c6e265050118a8738b7135cd65ce6ece619cb89
MoreiraGabriel <smoreira.gabriel@gmail.com>
2020-11-29 16:46:02 -0300

    Endpoint para listar resultado por id.

--- src/main/java/br/com/cesjf/enade/resource/ResultadoResource.java
+++ src/main/java/br/com/cesjf/enade/resource/ResultadoResource.java
@@ -1,19 +1,20 @@
 package br.com.cesjf.enade.resource;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cesjf.enade.dto.ResultadoDto;
 import br.com.cesjf.enade.service.ResultadoService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("resultado")
+@Api("Controller para resultado.")
 public class ResultadoResource {
 
 	@Autowired
@@ -21,7 +22,15 @@ public class ResultadoResource {
 	
 	@GetMapping("listar")
 	@ApiOperation(value = "Endpoint para listar os resultados")
-	public ResponseEntity<List<ResultadoDto>> listar(){
+	public ResponseEntity<?> listar(){
 		return ResponseEntity.ok(service.listar());
+	}
+	
+	@GetMapping("listar/{id}")
+	@ApiOperation(value = "Endpoint para listar os resultados")
+	public ResponseEntity<?> obterPorId(@PathVariable Long id){
+		
+		ResultadoDto dto = service.obterPorId(id);
+		return dto != null ? ResponseEntity.ok(dto) : ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/br/com/cesjf/enade/service/ResultadoService.java
+++ src/main/java/br/com/cesjf/enade/service/ResultadoService.java
@@ -1,10 +1,12 @@
 package br.com.cesjf.enade.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import br.com.cesjf.enade.dto.ResultadoDto;
+import br.com.cesjf.enade.model.Resultado;
 import br.com.cesjf.enade.repository.ResultadoRepository;
 
 @Service
@@ -14,5 +16,11 @@ public class ResultadoService {
 	
 	public List<ResultadoDto> listar(){
 		return ResultadoDto.converter(repository.findAll());
+	}
+
+	public ResultadoDto obterPorId(Long id) {
+		Optional<Resultado> resultado = repository.findById(id);
+		
+		return resultado.isPresent() ? new ResultadoDto(resultado.get()) : null;
 	}
 }
